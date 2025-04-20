bd2d2ebfbece74fb9ba7c5f96af84c49751ad678
Kamil Kloch <kamilk@Kamil-MBP16.local>
2025-04-21 00:15:30 +0200

    Added REcursiveRule.

--- src/test/scala/RecursiveRule.scala
+++ src/test/scala/RecursiveRule.scala
@@ -1,12 +1,15 @@
 import io.github.martinhh.derived.scalacheck.deriveArbitrary
-import org.scalacheck.Arbitrary
+import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatestplus.scalacheck.Checkers
+import io.github.martinhh.derived.RecursionFallback
 
 class RecursiveRule extends AnyFunSuite with Checkers {
   implicit override val generatorDrivenConfig: PropertyCheckConfiguration = PropertyCheckConfiguration(minSuccessful = 100)
 
-  given arbitraryTree: Arbitrary[Rule] = deriveArbitrary
+  given RecursionFallback[Rule] = RecursionFallback(Gen.oneOf[Rule](Rule.UseFirst, Rule.UseSecond))
+  
+  given Arbitrary[Rule] = deriveArbitrary
 
   test("test tree") {
     check { (rule: Rule) =>
