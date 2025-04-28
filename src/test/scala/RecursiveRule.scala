e09e972b0e71dcb7796781b4303191cd26febf87
Kamil Kloch <kamil.kloch@superfund-technologies.com>
2025-04-28 10:09:52 +0200

    Bumped scalacheck-derived to 0.8.2.

--- src/test/scala/RecursiveRule.scala
+++ src/test/scala/RecursiveRule.scala
@@ -6,17 +6,17 @@ import io.github.martinhh.derived.RecursionFallback
 
 class RecursiveRule extends AnyFunSuite with Checkers {
   implicit override val generatorDrivenConfig: PropertyCheckConfiguration = 
-    PropertyCheckConfiguration(minSuccessful = 100, sizeRange = 10)
+    PropertyCheckConfiguration(minSuccessful = 1000, sizeRange = 10)
 
   given RecursionFallback[Rule] = RecursionFallback(
-    Gen.oneOf[Rule](Rule.UseFirst, Rule.UseSecond).map {r => println("*"); r}
+    Gen.oneOf[Rule](Rule.UseFirst, deriveArbitrary[Rule.UseSecond].arbitrary)
   )
   
   given Arbitrary[Rule] = deriveArbitrary
 
   test("test tree") {
     check { (rule: Rule) =>
-      println(rule)
+//      println(rule)
       true
     }
   }
--- src/test/scala/Rule.scala
+++ src/test/scala/Rule.scala
@@ -3,7 +3,7 @@ sealed trait Rule
 object Rule {
   case object UseFirst extends Rule
 
-  case object UseSecond extends Rule
+  case class UseSecond(x: Int) extends Rule
 
   case class FirstOf(rules: Seq[Rule], default: Rule) extends Rule
 }
